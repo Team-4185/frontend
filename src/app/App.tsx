beee4b0f004ecf7c2fedb75a1dba11feb2efbc1b
blessed <qx.dicawl@gmail.com>
2025-09-30 11:47:41 +0300

    basic skeleton of the project

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="box">
+        <h1 className="hello">GADGET ROOM</h1>
+        <h3>WORK IN PROGRESS</h3>
+      </div>
+    </div>
+  );
+}
+
+export default App;

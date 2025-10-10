2741f53caa93e38533b51716ac2c3ed622f12890
blessed <qx.dicawl@gmail.com>
2025-10-10 18:11:20 +0300

    NTT-59/ShippingPage/PaymentPage

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,20 +1,9 @@
-import {
-  Container,
-  Box,
-  Grid,
-  Divider,
-  Accordion,
-  AccordionSummary,
-  Typography,
-  AccordionDetails,
-} from '@mui/material';
+import { Container, Box, Grid, Divider } from '@mui/material';
 import { Footer } from '../../shared/ui/Footer/Footer';
 import { Header } from '../../widgets/Header/Header';
+import { OrderSummary } from '../../shared/ui/Cart/OrderSummary';
 import RemoveIcon from '@mui/icons-material/Remove';
 import AddIcon from '@mui/icons-material/Add';
-import Lock from '/icons/lock.svg';
-import Voucher from '/icons/voucher.svg';
-import ExpandMoreIcon from '/icons/ExpandMore.svg';
 import './Cart.css';
 
 type CartProps = {
@@ -43,7 +32,7 @@ export const Cart: React.FC<CartProps> = ({ orderLength = 2 }) => {
 
   return (
     <>
-      <Header page="Cart" orderLength={0} product="" />
+      <Header page="Cart" orderLength={2} product="" />
       <Container disableGutters maxWidth="xl" className="cart-container">
         <Box className="cart-content">
           {/* Левая часть */}
@@ -96,61 +85,7 @@ export const Cart: React.FC<CartProps> = ({ orderLength = 2 }) => {
           </Box>
 
           {/* Правая часть */}
-          <Box className="cart-right">
-            <span className="cart-summary-title">Order Summary</span>
-            <Divider sx={{ mt: '31px', mb: '29px' }} />
-
-            <Box sx={{ display: 'flex', gap: '20px', flexDirection: 'column' }}>
-              <Box className="cart-summary-row">
-                <div className="cart-summary-label">Subtotal</div>
-                <div className="cart-summary-value">€ 875.50</div>
-              </Box>
-              <Box className="cart-summary-row">
-                <div className="cart-summary-label">Tax</div>
-                <div className="cart-summary-value">€ 00.00</div>
-              </Box>
-              <Box className="cart-summary-row">
-                <div className="cart-summary-label">Shipping</div>
-                <div className="cart-summary-value">€ 13.00</div>
-              </Box>
-            </Box>
-
-            <Divider sx={{ mt: '32px', mb: '49px' }} />
-
-            <Box className="cart-total">
-              <div className="cart-total-label">Total</div>
-              <div className="cart-total-value">€ 888.50</div>
-            </Box>
-
-            <button className="cart-btn-payment">
-              <img src={Lock} alt="Lock" /> continue to payment
-            </button>
-
-            <Accordion
-              sx={{
-                mt: '30px',
-                mb: '30px',
-                border: 'none',
-                boxShadow: 'none',
-                background: 'none',
-                '&:before': { display: 'none' },
-                alignItems: 'center',
-              }}
-            >
-              <AccordionSummary aria-controls="panel1-content" id="panel1-header">
-                <Typography className="cart-voucher-text">
-                  <img src={Voucher} alt="voucher" />
-                  <span className="cart-voucher-text">Use discount voucher</span>
-                  <span className="cart-voucher-optional">(optional)</span>
-                  <img src={ExpandMoreIcon} alt="Expand More" />
-                </Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus
-                ex, sit amet blandit leo lobortis eget.
-              </AccordionDetails>
-            </Accordion>
-          </Box>
+          <OrderSummary />
         </Box>
       </Container>
       <Footer />

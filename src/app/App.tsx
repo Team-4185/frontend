3a5b626ee413af0bdb2be789f00c4e51e076c971
blessed <qx.dicawl@gmail.com>
2025-10-16 17:29:29 +0300

    fix

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,3 @@
-import { Cart } from '../pages/Cart/Cart';
-
-import { Payment } from '../pages/Payment/Payment';
-
-import { Delivery } from '../pages/Delivery/Delivery';
-import { AuthPage } from '../pages/AuthPage/AuthPage';
-
 import './App.css';
 import { Catalog } from '../pages/Catalog/Catalog';
 
@@ -12,15 +5,6 @@ function App() {
   return (
     <>
       <Catalog />
-      {/* <hr style={{ height: '10px', background: 'black' }} />
-      <AuthPage />
-      <hr style={{ height: '10px', background: 'black' }} />
-      <Delivery />
-      <hr style={{ height: '10px', background: 'black' }} />
-      <Payment />
-      <hr style={{ height: '10px', background: 'black' }} />
-      <Cart />
-      <hr style={{ height: '10px', background: 'black' }} /> */}
     </>
   );
 }

4dfc3f96037ccfea867fd24cffbb1a8a0835ad1c
blessed <qx.dicawl@gmail.com>
2025-11-18 21:40:52 +0200

    fix api

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,20 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-  // const env = loadEnv(mode, process.cwd(), '');
-  // const target = env.VITE_BACKEND_ORIGIN || 'http://localhost:8080';
+  const env = loadEnv(mode, process.cwd(), '');
+  const target = env.VITE_BACKEND_ORIGIN || 'http://localhost:8080';
+
   return {
     plugins: [react()],
     server: {
-      host: '0.0.0.0',
-      port: 3000,
-      watch: {
-        usePolling: true,
+      proxy: {
+        // все запросы на /api будут перенаправляться на target
+        '/api': {
+          target,
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, ''), // если нужно убрать префикс
+        },
       },
     },
   };
